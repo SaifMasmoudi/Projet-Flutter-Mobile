a6d44c48c3bff89cf16588dedbbb78d3a0b26061
SaifMasmoudi <saifmasmoudi7@gmail.com>
2024-04-07 05:31:28 +0100

    version 05:31

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ import 'package:projetflutter/home.dart';
 
 
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 void main() => runApp(MyApp());
 
